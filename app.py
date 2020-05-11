ca99937dbbdb75aa0dfb75502541059a0607f818
Washington Assuncao <washington.assuncao@itbam.org.br>
2020-05-11 09:52:21 -0400

    GENESIS

---
+++ app.py
@@ -0,0 +1,36 @@
+import pickle
+from flask_basicauth import BasicAuth
+from flask import Flask, request, jsonify
+from textblob import TextBlob
+
+colunas = ['tamanho', 'ano', 'garagem']
+modelo = pickle.load(open('./modelos/modelo.sav', 'rb'))
+
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'washington'
+app.config['BASIC_AUTH_PASSWORD'] = 'mlops'
+
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/sentimento/<frase>')
+@basic_auth.required
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(to='en')
+    polaridade = tb_en.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
+@app.route('/cotacao/', methods=['POST'])
+@basic_auth.required
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+    return jsonify(preco=preco[0])
+
+if __name__ == '__main__':
+    app.run(debug=True)
